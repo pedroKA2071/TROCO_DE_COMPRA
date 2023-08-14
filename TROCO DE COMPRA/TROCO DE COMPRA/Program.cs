aa764714b9e3dc0e188247e3ee07e7c2fc06a961
Pedro Henrique Gaspar <mortadela22112005@gmail.com>
2023-08-14 09:35:53 -0300

    ativ 2 troco de compra

---
+++ TROCO DE COMPRA/TROCO DE COMPRA/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TROCO_DE_COMPRA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int  troco, total, troco_restante;
+
+            Console.WriteLine("qual o valor total da compra ?");
+
+            total = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine(" qual o valor que ira pagar ? ");
+
+            troco = int.Parse(Console.ReadLine());
+
+            troco_restante = troco - total;
+           
+
+            if (total > troco)
+            {
+
+                Console.WriteLine("Não e possivel pagar");
+            } else
+            {
+                Console.WriteLine("e possivel pagar e seu troco e de " + troco_restante);
+
+            }
+
+               
+
+               
+           
+
+
+
+
+            Console.ReadKey();  
+
+        }
+    }
+}
